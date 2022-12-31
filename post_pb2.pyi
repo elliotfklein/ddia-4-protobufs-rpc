41bc350f80cfaaeada9cb6b552d02994fc8b21e8
Elliot Klein <elliotfklein@gmail.com>
2022-12-31 13:28:38 -0500

    Regen protobuf code

--- post_pb2.pyi
+++ post_pb2.pyi
@@ -16,22 +16,20 @@ class GetPostsRequest(_message.Message):
     def __init__(self) -> None: ...
 
 class Post(_message.Message):
-    __slots__ = ["a_new_field", "author_id", "body", "id", "likes", "tags", "title"]
+    __slots__ = ["author_id", "body", "id", "likes", "tags", "title"]
     AUTHOR_ID_FIELD_NUMBER: _ClassVar[int]
-    A_NEW_FIELD_FIELD_NUMBER: _ClassVar[int]
     BODY_FIELD_NUMBER: _ClassVar[int]
     ID_FIELD_NUMBER: _ClassVar[int]
     LIKES_FIELD_NUMBER: _ClassVar[int]
     TAGS_FIELD_NUMBER: _ClassVar[int]
     TITLE_FIELD_NUMBER: _ClassVar[int]
-    a_new_field: bool
     author_id: str
     body: str
     id: int
     likes: int
     tags: _containers.RepeatedScalarFieldContainer[str]
     title: str
-    def __init__(self, id: _Optional[int] = ..., title: _Optional[str] = ..., body: _Optional[str] = ..., author_id: _Optional[str] = ..., likes: _Optional[int] = ..., a_new_field: bool = ..., tags: _Optional[_Iterable[str]] = ...) -> None: ...
+    def __init__(self, id: _Optional[int] = ..., title: _Optional[str] = ..., body: _Optional[str] = ..., author_id: _Optional[str] = ..., likes: _Optional[int] = ..., tags: _Optional[_Iterable[str]] = ...) -> None: ...
 
 class PostDB(_message.Message):
     __slots__ = ["posts"]
